3c26515df42cbbc13ca2a74358f82e12eba66025
Aryan Shrivastava <aryanshrivastava@Aryans-MacBook-Air-2.local>
2026-04-02 11:39:43 -0500

    update vite to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/imc-prosperity-4-visualizer/',
+  base: '/prosperity4-abacus-visualizer/',
   build: {
     minify: false,
     sourcemap: true,
